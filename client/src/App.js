8a0512a7160c06b75a66fe979cf7263bc2ec78d0
palminski <williambolls98@gmail.com>
2023-04-24 12:29:09 -0700

    initial setup complete

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,9 @@ import Cards from './pages/Cards';
 import Review from './pages/Review';
 import LoggedOut from './pages/LoggedOut';
 
+//------[Router]----------------------
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 //------[Set Up Apollo]---------------
 const httpLink = createHttpLink({
   //This can be changed to '/graphql' if using proxy in package.json
@@ -71,3 +74,53 @@ function App() {
 }
 
 export default App;
+
+{/* <div className="App">
+      <Nav pageSelected={pageSelected} setPageSelected={setPageSelected}/>
+
+      {pageSelected === 'Home' && <Home/>}
+      {pageSelected === 'Log In' && <Login setPageSelected={setPageSelected}/>}
+      {pageSelected === 'Sign Up' && <Signup setPageSelected={setPageSelected}/>}
+      {Auth.loggedIn() ?
+      <>
+      {pageSelected === 'Decks' && <Decks/>}
+      {pageSelected === 'Cards' && <Cards/>}
+      {pageSelected === 'Review' && <Review/>}
+      </>
+      :
+      <>
+      {pageSelected === 'Decks' && <LoggedOut/>}
+      {pageSelected === 'Cards' && <LoggedOut/>}
+      {pageSelected === 'Review' && <LoggedOut/>}
+      </>
+      }
+
+    </div> */}
+
+    // <ApolloProvider client={client}>
+    //   <div className="App">
+    //     <Nav pageSelected={pageSelected} setPageSelected={setPageSelected} />
+    //     <Routes>
+    //       <Route path="/" element={<Home />} />
+    //       <Route path="/login" element={<Login />} />
+    //       <Route path="/signup" element={<Signup />} />
+
+    //       {Auth.loggedIn() ?
+    //         <>
+    //           <Route path="/decks" element={<Decks />} />
+    //           <Route path="/cards" element={<Cards />} />
+    //           <Route path="/review" element={<Review />} />
+    //         </>
+    //         :
+    //         <>
+    //           <Route path="/decks" element={<LoggedOut />} />
+    //           <Route path="/cards" element={<LoggedOut />} />
+    //           <Route path="/review" element={<LoggedOut />} />
+    //         </>
+    //       }
+    //        <Route path="*" element={<NotFound />}></Route>
+    //     </Routes>
+
+
+    //   </div>
+    // </ApolloProvider>
