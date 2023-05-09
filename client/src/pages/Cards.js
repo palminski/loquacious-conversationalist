05b9969e1d1d7919ad24a238ef6b2573aa39b6f4
palminski <williambolls98@gmail.com>
2023-05-09 12:49:19 -0700

    set up page to align next to cards list

--- client/src/pages/Cards.js
+++ client/src/pages/Cards.js
@@ -14,7 +14,7 @@ const Cards = () => {
     //===[Redux]==============================================
     const dispatch = useDispatch();
     const deck = useSelector(selectDeck);
-    
+
 
     //===[Queries]============================================
     const { refetch } = useQuery(QUERY_CURRENT_USER);
@@ -29,7 +29,7 @@ const Cards = () => {
 
     const [selectedCard, setSelectedCard] = useState(null)
 
-   const [modalOpen,setModalOpen] = useState(false);
+    const [modalOpen, setModalOpen] = useState(false);
 
     //===[Functions]==========================================
     function handleFormChange(e) {
@@ -50,11 +50,11 @@ const Cards = () => {
                     optimisticResponse: {
                         addCard: {
                             _id: -1,
-                        __typename: 'Card',
-                        sideATitle: "test",
-                        sideBTitle: formState.sideBTitle,
-                        sideADescription: formState.sideADescription,
-                        sideBDescription: formState.sideBDescription
+                            __typename: 'Card',
+                            sideATitle: "test",
+                            sideBTitle: formState.sideBTitle,
+                            sideADescription: formState.sideADescription,
+                            sideBDescription: formState.sideBDescription
                         }
                     }
                 });
@@ -62,10 +62,10 @@ const Cards = () => {
                 refetch();
                 setSelectedCard(null);
                 const newCard = mutationResponse.data.addCard;
-                const updatedCardArray = [...deck.cards,newCard];
-                
+                const updatedCardArray = [...deck.cards, newCard];
+
                 dispatch(updateCards(updatedCardArray));
-                
+
             }
             else {
                 const mutationResponse = await editCard({
@@ -81,7 +81,7 @@ const Cards = () => {
                 setFormState({ sideATitle: '', sideADescription: '', sideBTitle: '', sideBDescription: '' });
                 refetch();
                 setSelectedCard(null);
-                
+
                 const newCard = mutationResponse.data.editCard;
                 const updatedCardArray = [...deck.cards];
                 updatedCardArray[updatedCardArray.findIndex((card) => card._id === newCard._id)] = newCard;
@@ -113,7 +113,7 @@ const Cards = () => {
             const updatedCardArray = [...deck.cards];
 
             let index = updatedCardArray.findIndex((card) => card._id === deletedCard._id)
-            if (index !== -1) updatedCardArray.splice(index,1);
+            if (index !== -1) updatedCardArray.splice(index, 1);
 
             dispatch(updateCards(updatedCardArray));
         }
@@ -126,8 +126,7 @@ const Cards = () => {
             document.body.style.overflow = "hidden";
             document.body.style.height = "100%";
         }
-        else
-        {
+        else {
             document.body.style.overflow = "auto";
             document.body.style.height = "auto";
         };
@@ -140,71 +139,77 @@ const Cards = () => {
     return (
         <div className="grow-in">
 
-            { deck.title ? 
+            {deck.title ?
                 <>
+                    {/* NEW CARD FORM */}
                     <div className='container'>
                         <div className='new-card-form'>
                             <h2 className='white'>{selectedCard ? "Edit card for " : "Add card to "}{deck.title}</h2>
                             {deck.description && <h3 className='description white'>{deck.description}</h3>}
-
                             <form>
                                 <div className='flex-left edit-card'>
                                     <div className='side-form side-a'>
                                         <h2>Front</h2>
                                         <label htmlFor="sideATitle">Side A Title</label>
-                                        <input required={true} type="text" id="sideATitle" name="sideATitle"  onChange={handleFormChange} value={formState.sideATitle}></input>
+                                        <input required={true} type="text" id="sideATitle" name="sideATitle" onChange={handleFormChange} value={formState.sideATitle}></input>
                                         <br />
                                         <label htmlFor="sideADescription">Side A Description</label>
                                         <br />
-                                        <textarea rows={7} required={true} type="text" id="sideADescription" name="sideADescription"  onChange={handleFormChange} value={formState.sideADescription}></textarea>
+                                        <textarea rows={7} required={true} type="text" id="sideADescription" name="sideADescription" onChange={handleFormChange} value={formState.sideADescription}></textarea>
                                     </div>
                                     <div className='side-form side-b'>
                                         <h2>Back</h2>
                                         <label htmlFor="sideBTitle">Side B Title</label>
-                                        <input required={true} type="text" id="sideBTitle" name="sideBTitle"  onChange={handleFormChange} value={formState.sideBTitle}></input>
+                                        <input required={true} type="text" id="sideBTitle" name="sideBTitle" onChange={handleFormChange} value={formState.sideBTitle}></input>
                                         <br />
                                         <label htmlFor="sideBDescription">Side B Description</label>
                                         <br />
-                                        <textarea rows={7} required={true} type="text" id="sideBDescription" name="sideBDescription"  onChange={handleFormChange} value={formState.sideBDescription}></textarea>
+                                        <textarea rows={7} required={true} type="text" id="sideBDescription" name="sideBDescription" onChange={handleFormChange} value={formState.sideBDescription}></textarea>
                                     </div>
                                 </div>
                                 <button className='add-card-button' onClick={handleFormSubmit}>{selectedCard ? "Save Card" : "Add Card"}</button>
                                 {selectedCard && <button className='add-card-button' onClick={handleDeleteCard}>Delete Card</button>}
                             </form>
-
-
                         </div>
                     </div>
+                    <div className='container' >
+                        <div className='flex-left'>
 
-                    <div className='container '>
-                        {(deck.cards.length > 0) &&
-                            <ul className='card-list'>
-                                <h2>Cards in {deck.title}</h2>
-                                {deck.cards.map(card => (
-                                    <li onClick={() => { setSelectedCard(card); setFormState({ sideATitle: card.sideATitle, sideADescription: card.sideADescription, sideBTitle: card.sideBTitle, sideBDescription: card.sideBDescription }); }} key={card._id} className={`${(selectedCard?._id === card._id) && "selected-card"}`}>
+                            {(deck.cards.length > 0) &&
+                                <ul className='card-list' style={{ flex: 1 }}>
+                                    <h2>Cards in {deck.title}</h2>
+                                    {deck.cards.map(card => (
+                                        <li onClick={() => { setSelectedCard(card); setFormState({ sideATitle: card.sideATitle, sideADescription: card.sideADescription, sideBTitle: card.sideBTitle, sideBDescription: card.sideBDescription }); }} key={card._id} className={`${(selectedCard?._id === card._id) && "selected-card"}`}>
 
-                                        <h3>{card.sideATitle} - {card.sideBTitle} </h3>
-                                    </li>
-                                ))}
-                            </ul>
-                        }
+                                            <h3>{card.sideATitle} - {card.sideBTitle} </h3>
+                                        </li>
+                                    ))}
+                                </ul>
+                            }
+                        
+                        <div className='sharables' style={{ flex: 1 }}>
+                            <h2><button onClick={() => { navigator.clipboard.writeText(sharedDeckURL + deck._id) }}>Copy Shareable Link to CLipboard</button></h2>
+
+                            <QRCodeSVG value={sharedDeckURL + deck._id} onClick={toggleModal} style={{ cursor: 'zoom-in' }} imageSettings={{ excavate: false }} />
+                        </div>
+                        </div>
+
+                        {/* This is where QR code iwll go for now */}
                     </div>
-                    <h2><button onClick={() => {navigator.clipboard.writeText(sharedDeckURL+deck._id)}}>Copy Shareable Link to CLipboard</button></h2>
-                    
-                    <QRCodeSVG value={sharedDeckURL+deck._id} onClick={toggleModal} style={{cursor: 'zoom-in'}} imageSettings={{excavate: false}}/>
-                    {/* This is where QR code iwll go for now */}
-                    {modalOpen && <QRCodeModal toggleModal={toggleModal}  link={sharedDeckURL+deck._id}/>}
+
+
+                    {modalOpen && <QRCodeModal toggleModal={toggleModal} link={sharedDeckURL + deck._id} />}
                 </>
                 :
-                        <div className='container'>
-                            <div className='no-deck-warning'>
-                                <h1>No Deck Selected</h1>
-                                <hr></hr>
-                                <h2>Please select a deck to either review or add cards to.</h2>
-                            </div>
+                <div className='container'>
+                    <div className='no-deck-warning'>
+                        <h1>No Deck Selected</h1>
+                        <hr></hr>
+                        <h2>Please select a deck to either review or add cards to.</h2>
+                    </div>
 
-                        </div>
-                
+                </div>
+
             }
 
 
