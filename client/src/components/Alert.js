94a79271fed3531ec546fa14cc9163f58dd1b033
palminski <williambolls98@gmail.com>
2023-10-12 09:25:34 -0700

    updated styles and added alerts for login signup failusres

---
+++ client/src/components/Alert.js
@@ -0,0 +1,16 @@
+
+
+const Alert = ({message}) => {
+
+    return(
+        <>
+            
+            <div className="alert grow-in">
+                <h3>{message}</h3>
+            </div>
+
+
+        </>
+    )
+}
+export default Alert;
